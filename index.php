734c5830a65e21e006ada143bc9857d5a3c74a02
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-06-29 10:18:25 +0200

    ajout d'un tableau

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+include('includes/header.php');
+?>
+
+<main>
+<?php 
+$tableau = array();
+$tableau = [];
+$tableau = [true, 45, 'Terre', [45, 46, 47]];
+
+print_r($tableau)
+?>
+</main>
+
+<?php
+include('includes/footer.php');

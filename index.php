dd7549d55f93e3c43227fc94f365faa0069acfcd
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-06-29 11:25:07 +0200

    calcul de la moyenne d'un tableau

--- index.php
+++ index.php
@@ -16,7 +16,16 @@ array_push($tableau, 'Voiture');
 array_push($tableau, 'Voiture 2', 'Tortue Ninja');
 
 debug($tableau[2]);
-debug($tableau[2]);
+
+echo count($tableau);
+
+echo "<br />";
+$notes = [12, 5, 16, 7];
+// Afficher la moyenne des notes 
+$sommesNotes = $notes[0] + $notes[1] + $notes[2] + $notes[3];
+$moyennesNotes = $sommesNotes / count($notes);
+
+echo $moyennesNotes
 
 ?>
 </main>

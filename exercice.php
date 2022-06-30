3200d2511c9b44c83f32d3494cc435a5370d3901
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-06-30 11:21:20 +0200

    Consigne de l'exercice

---
+++ exercice.php
@@ -0,0 +1,16 @@
+/* 
+- Créer un tableau php avec ses nombres : 
+27, 15, 34, 379, 248, 5643, 81, 211, 999, 142,, 300, 572
+
+- tester les nombres de ce tableau pour savoir si chaque 
+nombre est pair ou impair (un nb est âir si le résultat de la division
+par 2 est 0)
+
+- Afficher les résultats sous forme de liste html 'ul' comme ceci 
+(xx, yy sont les nombres du tableau) :
+
+* xx : pair 
+* yy : import_request_variables
+...
+
+*/

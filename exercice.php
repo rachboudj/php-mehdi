0e3c7b8e2921c29cd63f8a3fc5f275847ea3d86f
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-01 13:58:12 +0200

    affichage du premier élément de l'API en html

--- exercice.php
+++ exercice.php
@@ -72,7 +72,31 @@ foreach($nombres as $nombre) {
 debug("Le nombre maximum est : $index");
 
 $users = file_get_contents("./data/users.json");
+$users_array_phps = json_decode($users, true);
+// foreach($users_array_phps as $users_array_php) {
+//     echo $users_array_phps;
+// }
+// debug($users_array_php);
 
-debug($users);
+debug($users_array_phps);
+
+// debug($users_array_php['results'][$key]['gender']);
+// echo "Genre : " . $users_array_php['results'][0]['gender'];
+// echo $users_array_php['results'][0]['gender'];
+
+
+
+// Dans une card; ajouter une photo, nom, prénom, email, date 
+// de naissance, adresse, téléphone
 
 ?>
+<div class="container-card">
+    <div class="card">
+    <img src="" alt="">
+    <h3>Nom : <?php echo $users_array_phps['results'][0]['name']['last']; ?>; Prénom : <?php echo $users_array_phps['results'][0]['name']['first'] ?></h3>
+    <h4>Email : <?php echo $users_array_phps['results'][0]['email'] ?></h4>
+    <p class="naissance"><?php echo $users_array_phps['results'][0]['registered']['date'] ?></p>
+    <p class="adresse"><?php echo $users_array_phps['results'][0]['location']['street']['number'] ?>, <?php echo $users_array_phps['results'][0]['location']['street']['name'] ?></p>
+    <p class="telephone"><?php echo $users_array_phps['results'][0]['phone'] ?></p>
+    </div>
+</div>
